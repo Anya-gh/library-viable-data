d5edc9d51c124112296d5165fac2c508ab979851
Anya-gh <Aniket.Singh@warwick.ac.uk>
2023-08-30 23:13:35 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+python -m venv ./backend/env
+source backend/env/bin/activate
 pip install -r requirements.txt
 npm --prefix ./frontend install
 npm --prefix ./frontend run dev & python backend/src/api.py
